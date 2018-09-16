8e27e93e06f97eb538301a5adca76ec5e694b726
ThanhVT <vutienthanh0210@gmail.com>
2018-09-16 10:49:02 +0700

    Report max từ đến

--- app/src/main/java/com/traderpro/thanhvt/ReportActivity.java
+++ app/src/main/java/com/traderpro/thanhvt/ReportActivity.java
@@ -58,6 +58,8 @@ public class ReportActivity extends AppCompatActivity {
         int case6_vol1 = 0, case6_vol2 = 0, case6_vol3 = 0, case6_vol4 = 0, case6_vol5 = 0, case6_vol6 = 0;
 
         int countThua1 = 0, countThua2 = 0, countThua3 = 0;
+        double maxt1 = 100, maxd1 = 0, maxt2 = 100, maxd2 = 0, maxt3 = 100, maxd3 = 0, maxt4 = 100, maxd4 = 0, maxt5 = 100, maxd5 = 0, maxt6 = 100, maxd6 = 0,
+                maxt7 = 100, maxd7 = 0, maxt8 = 100, maxd8 = 0, maxt9 = 100, maxd9 = 0, maxt10 = 100, maxd10 = 0, maxt11 = 100, maxd11 = 0, maxt12 = 100, maxd12 = 0;
 
         for (NotificationEntity en : lstEntity) {
             Double giaBao = Double.parseDouble(en.strGia);
@@ -65,7 +67,7 @@ public class ReportActivity extends AppCompatActivity {
             Double giaMin = en.strGiaMin;
             Double volHT = Double.parseDouble(en.strVol);
             Double volTB = Double.parseDouble(en.strVolTB);
-
+            Double dProfit = ((en.strGiaMax - giaBao) / giaBao) * 100;
             //
             if (giaMin != null) {
                 if (giaMin < giaBao * 0.995) {
@@ -88,39 +90,63 @@ public class ReportActivity extends AppCompatActivity {
 
                 if (en.strCase.equals("1")) {
                     thang1++;
+                    if (dProfit < maxt1) maxt1 = dProfit;
+                    if (dProfit > maxd1) maxd1 = dProfit;
                 }
                 if (en.strCase.equals("2")) {
                     thang2++;
+                    if (dProfit < maxt2) maxt2 = dProfit;
+                    if (dProfit > maxd2) maxd2 = dProfit;
                 }
                 if (en.strCase.equals("3")) {
                     thang3++;
+                    if (dProfit < maxt3) maxt3 = dProfit;
+                    if (dProfit > maxd3) maxd3 = dProfit;
                 }
                 if (en.strCase.equals("4")) {
                     thang4++;
+                    if (dProfit < maxt4) maxt4 = dProfit;
+                    if (dProfit > maxd4) maxd4 = dProfit;
                 }
                 if (en.strCase.equals("5")) {
                     thang5++;
+                    if (dProfit < maxt5) maxt5 = dProfit;
+                    if (dProfit > maxd5) maxd5 = dProfit;
                 }
                 if (en.strCase.equals("6")) {
                     thang6++;
+                    if (dProfit < maxt6) maxt6 = dProfit;
+                    if (dProfit > maxd6) maxd6 = dProfit;
                 }
                 if (en.strCase.equals("7")) {
                     thang7++;
+                    if (dProfit < maxt7) maxt7 = dProfit;
+                    if (dProfit > maxd7) maxd7 = dProfit;
                 }
                 if (en.strCase.equals("8")) {
                     thang8++;
+                    if (dProfit < maxt8) maxt8 = dProfit;
+                    if (dProfit > maxd8) maxd8 = dProfit;
                 }
                 if (en.strCase.equals("9")) {
                     thang9++;
+                    if (dProfit < maxt9) maxt9 = dProfit;
+                    if (dProfit > maxd9) maxd9 = dProfit;
                 }
                 if (en.strCase.equals("10")) {
                     thang10++;
+                    if (dProfit < maxt10) maxt10 = dProfit;
+                    if (dProfit > maxd10) maxd10 = dProfit;
                 }
                 if (en.strCase.equals("11")) {
                     thang11++;
+                    if (dProfit < maxt11) maxt11 = dProfit;
+                    if (dProfit > maxd11) maxd11 = dProfit;
                 }
                 if (en.strCase.equals("12")) {
                     thang12++;
+                    if (dProfit < maxt12) maxt12 = dProfit;
+                    if (dProfit > maxd12) maxd12 = dProfit;
                 }
             }
             if (en.strCase.equals("1") && en.strGiaMax > 0) {
@@ -269,35 +295,35 @@ public class ReportActivity extends AppCompatActivity {
             tvThua2.setText(countThua2 + " lần");
             tvThua3.setText(countThua3 + " lần");
 
-            tvThangThua.setText(countThangThua + "/" + strTinhToan + " lần báo");
-            tvThangNua.setText(countThangNua + "/" + strTinhToan + " lần báo");
-            tvTT1.setText(thang1 + "/" + tong1 + " lần báo");
-            tvTT2.setText(thang2 + "/" + tong2 + " lần báo");
-            tvTT3.setText(thang3 + "/" + tong3 + " lần báo");
-            tvTT4.setText(thang4 + "/" + tong4 + " lần báo");
-            tvTT5.setText(thang5 + "/" + tong5 + " lần báo");
-            tvTT6.setText(thang6 + "/" + tong6 + " lần báo");
-            tvTT7.setText(thang7 + "/" + tong7 + " lần báo");
-            tvTT8.setText(thang8 + "/" + tong8 + " lần báo");
-            tvTT9.setText(thang9 + "/" + tong9 + " lần báo");
-            tvTT10.setText(thang10 + "/" + tong10 + " lần báo");
-            tvTT11.setText(thang11 + "/" + tong11 + " lần báo");
-            tvTT12.setText(thang12 + "/" + tong12 + " lần báo");
+            tvThangThua.setText(countThangThua + "/" + strTinhToan + " lần");
+            tvThangNua.setText(countThangNua + "/" + strTinhToan + " lần");
+            tvTT1.setText(thang1 + "/" + tong1 + " lần: " + String.format("%.1f", maxt1 == 100 ? 0 : maxt1) + "% >>> " + String.format("%.1f", maxd1) + "%");
+            tvTT2.setText(thang2 + "/" + tong2 + " lần: " + String.format("%.1f", maxt2 == 100 ? 0 : maxt2) + "% >>> " + String.format("%.1f", maxd2) + "%");
+            tvTT3.setText(thang3 + "/" + tong3 + " lần: " + String.format("%.1f", maxt3 == 100 ? 0 : maxt3) + "% >>> " + String.format("%.1f", maxd3) + "%");
+            tvTT4.setText(thang4 + "/" + tong4 + " lần: " + String.format("%.1f", maxt4 == 100 ? 0 : maxt4) + "% >>> " + String.format("%.1f", maxd4) + "%");
+            tvTT5.setText(thang5 + "/" + tong5 + " lần: " + String.format("%.1f", maxt5 == 100 ? 0 : maxt5) + "% >>> " + String.format("%.1f", maxd5) + "%");
+            tvTT6.setText(thang6 + "/" + tong6 + " lần: " + String.format("%.1f", maxt6 == 100 ? 0 : maxt6) + "% >>> " + String.format("%.1f", maxd6) + "%");
+            tvTT7.setText(thang7 + "/" + tong7 + " lần: " + String.format("%.1f", maxt7 == 100 ? 0 : maxt7) + "% >>> " + String.format("%.1f", maxd7) + "%");
+            tvTT8.setText(thang8 + "/" + tong8 + " lần: " + String.format("%.1f", maxt8 == 100 ? 0 : maxt8) + "% >>> " + String.format("%.1f", maxd8) + "%");
+            tvTT9.setText(thang9 + "/" + tong9 + " lần: " + String.format("%.1f", maxt9 == 100 ? 0 : maxt9) + "% >>> " + String.format("%.1f", maxd9) + "%");
+            tvTT10.setText(thang10 + "/" + tong10 + " lần: " + String.format("%.1f", maxt10 == 100 ? 0 : maxt10) + "% >>> " + String.format("%.1f", maxd10) + "%");
+            tvTT11.setText(thang11 + "/" + tong11 + " lần: " + String.format("%.1f", maxt11 == 100 ? 0 : maxt11) + "% >>> " + String.format("%.1f", maxd11) + "%");
+            tvTT12.setText(thang12 + "/" + tong12 + " lần: " + String.format("%.1f", maxt12 == 100 ? 0 : maxt12) + "% >>> " + String.format("%.1f", maxd12) + "%");
 
             tvTitleThang1.setText("Tỷ lệ thắng > 0.5%");
             tvTitleThang2.setText("Tỷ lệ thắng > 1%");
-            tvTitle1.setText("Thuật toán 1 thành công");
-            tvTitle2.setText("Thuật toán 2 thành công");
-            tvTitle3.setText("Thuật toán 3 thành công");
-            tvTitle4.setText("Thuật toán 4 thành công");
-            tvTitle5.setText("Thuật toán 5 thành công");
-            tvTitle6.setText("Thuật toán 6 thành công");
-            tvTitle7.setText("Thuật toán 7 thành công");
-            tvTitle8.setText("Thuật toán 8 thành công");
-            tvTitle9.setText("Thuật toán 9 thành công");
-            tvTitle10.setText("Thuật toán 10 thành công");
-            tvTitle11.setText("Thuật toán 11 thành công");
-            tvTitle12.setText("Thuật toán 12 thành công");
+            tvTitle1.setText("Thuật toán 1 thắng");
+            tvTitle2.setText("Thuật toán 2 thắng");
+            tvTitle3.setText("Thuật toán 3 thắng");
+            tvTitle4.setText("Thuật toán 4 thắng");
+            tvTitle5.setText("Thuật toán 5 thắng");
+            tvTitle6.setText("Thuật toán 6 thắng");
+            tvTitle7.setText("Thuật toán 7 thắng");
+            tvTitle8.setText("Thuật toán 8 thắng");
+            tvTitle9.setText("Thuật toán 9 thắng");
+            tvTitle10.setText("Thuật toán 10 thắng");
+            tvTitle11.setText("Thuật toán 11 thắng");
+            tvTitle12.setText("Thuật toán 12 thắng");
 
             tvTitleAn1.setText("Tỷ lệ ăn ~ 1%");
             tvTitleAn2.setText("Tỷ lệ ăn ~ 2%");
@@ -315,20 +341,20 @@ public class ReportActivity extends AppCompatActivity {
             tvThua2.setText(countThua2 + " times");
             tvThua3.setText(countThua3 + " times");
 
-            tvThangThua.setText(countThangThua + "/" + strTinhToan + " note times");
-            tvThangNua.setText(countThangNua + "/" + strTinhToan + " note times");
-            tvTT1.setText(thang1 + "/" + tong1 + " note times");
-            tvTT2.setText(thang2 + "/" + tong2 + " note times");
-            tvTT3.setText(thang3 + "/" + tong3 + " note times");
-            tvTT4.setText(thang4 + "/" + tong4 + " note times");
-            tvTT5.setText(thang5 + "/" + tong5 + " note times");
-            tvTT6.setText(thang6 + "/" + tong6 + " note times");
-            tvTT7.setText(thang7 + "/" + tong7 + " note times");
-            tvTT8.setText(thang8 + "/" + tong8 + " note times");
-            tvTT9.setText(thang9 + "/" + tong9 + " note times");
-            tvTT10.setText(thang10 + "/" + tong10 + " note times");
-            tvTT11.setText(thang11 + "/" + tong11 + " note times");
-            tvTT12.setText(thang12 + "/" + tong12 + " note times");
+            tvThangThua.setText(countThangThua + "/" + strTinhToan + " note");
+            tvThangNua.setText(countThangNua + "/" + strTinhToan + " note");
+            tvTT1.setText(thang1 + "/" + tong1 + " note");
+            tvTT2.setText(thang2 + "/" + tong2 + " note");
+            tvTT3.setText(thang3 + "/" + tong3 + " note");
+            tvTT4.setText(thang4 + "/" + tong4 + " note");
+            tvTT5.setText(thang5 + "/" + tong5 + " note");
+            tvTT6.setText(thang6 + "/" + tong6 + " note");
+            tvTT7.setText(thang7 + "/" + tong7 + " note");
+            tvTT8.setText(thang8 + "/" + tong8 + " note");
+            tvTT9.setText(thang9 + "/" + tong9 + " note");
+            tvTT10.setText(thang10 + "/" + tong10 + " note");
+            tvTT11.setText(thang11 + "/" + tong11 + " note");
+            tvTT12.setText(thang12 + "/" + tong12 + " note");
 
             tvTitleThang1.setText("Rate win > 0.5%");
             tvTitleThang2.setText("Rate win > 1%");
